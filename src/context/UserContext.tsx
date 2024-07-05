5448c63b5aba37901215779769ffdd6c4b0bab18
rambak02 <rambak02@mail.ru>
2024-07-05 07:54:04 +0700

    N11

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -44,7 +44,7 @@ export const UserProvider: FC<UserProviderProps> = ({ children }) => {
     const fetchUser = async () => {
       try {
         const tg = window.Telegram.WebApp
-        const initData = `${JSON.parse(JSON.stringify(tg.initData))}`
+        const initData = `${JSON.stringify(tg.initData)}`
         const JSONresponse = await AuthService.initDataUser(initData);
         const response = JSON.stringify(JSONresponse.data);
         const tokens = JSON.parse(response);
