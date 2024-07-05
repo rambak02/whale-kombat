9627a1c9c9472dcff59638e7171a05acd784ddee
rambak02 <rambak02@mail.ru>
2024-07-05 07:38:49 +0700

    N9

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -47,7 +47,7 @@ export const UserProvider: FC<UserProviderProps> = ({ children }) => {
         alert(JSON.stringify(tg.initData))
         alert("query_id=AAFYCeMcAAAAAFgJ4xzSwQ8I&user=%7B%22id%22%3A484641112%2C%22first_name%22%3A%22%D0%A0%D0%B0%D0%BC%D0%B0%D0%BB%22%2C%22last_name%22%3A%22%D0%91%D0%B0%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%22%2C%22username%22%3A%22rabltrabl%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1720097864&hash=3c17818991ce5d3de2628092c1cff31a70bbb35002a9c6317c9cba40161237b5")
        alert(JSON.parse(tg.initData))
-        const initData = "query_id=AAFYCeMcAAAAAFgJ4xzSwQ8I&user=%7B%22id%22%3A484641112%2C%22first_name%22%3A%22%D0%A0%D0%B0%D0%BC%D0%B0%D0%BB%22%2C%22last_name%22%3A%22%D0%91%D0%B0%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%22%2C%22username%22%3A%22rabltrabl%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1720097864&hash=3c17818991ce5d3de2628092c1cff31a70bbb35002a9c6317c9cba40161237b5"
+        // const initData = "query_id=AAFYCeMcAAAAAFgJ4xzSwQ8I&user=%7B%22id%22%3A484641112%2C%22first_name%22%3A%22%D0%A0%D0%B0%D0%BC%D0%B0%D0%BB%22%2C%22last_name%22%3A%22%D0%91%D0%B0%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%22%2C%22username%22%3A%22rabltrabl%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1720097864&hash=3c17818991ce5d3de2628092c1cff31a70bbb35002a9c6317c9cba40161237b5"
         const JSONresponse = await AuthService.initDataUser(JSON.parse(tg.initData));
         const response = JSON.stringify(JSONresponse.data);
         const tokens = JSON.parse(response);
